a39c94cf5d46d56c17c760363504704ba32741ca
deadmagic161 <fernando093@yandex.ru>
2017-04-19 01:30:35 +0700

    add files

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,60 @@
-// Include gulp
 var gulp = require('gulp'),
-  // Include Our Plugins
-  less = require('gulp-less'),
   postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer');
+  autoprefixer = require('autoprefixer'),
+  rename = require ('gulp-rename'),
+  short = require('postcss-short'),
+  assets = require ('postcss-assets'),
+  browserSync = require('browser-sync').create();
 
-gulp.task('css', function () {
-  gulp
-    .src('./less/main.less')
-    .pipe(less({ strictMath: true }))
-    .pipe(postcss([
-      autoprefixer({ browsers: ['> 1%', 'IE 9', 'IE 10']})
-    ]))
-    .pipe(gulp.dest('./css'));
-});
-
-gulp.task('watch', function () {
-  gulp.watch('./less/**/*.less', ['css']);
-});
 
 gulp.task('default', ['css', 'watch']);
+
+
+gulp.task('default', ['dev']);
+gulp.task('dev', ['build-dev', 'browser-sync', 'watch']);
+
+gulp.task('build-dev',['html', 'css-dev', 'assets']);
+
+gulp.task('css-dev', function() {
+  var processors = [
+    short,
+    assets ({
+      loadPath: ['src/assets/img'],
+      relatiTo: 'src/styles/'
+    }),
+  ];
+    return gulp.src('./src/styles/*.css')
+          .pipe(postcss(processors))
+          .pipe(rename('styleOut.css'))
+          .pipe(postcss([
+              autoprefixer({ browsers: ['> 1%', 'IE 9', 'IE 10']})
+          ]))
+          .pipe(gulp.dest('./build/styles/'));
+});
+
+gulp.task('html', function () {
+  return gulp.src('./src/index.html')
+    .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('browser-sync', function() {
+  return browserSync.init({
+    server: {
+      baseDir: './build/'
+    }
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/styles/*.css', ['css-dev']);
+  gulp.watch('./src/index.html', ['html']);
+  gulp.watch('./src/**/*.*', browserSync.reload);
+});
+
+
+gulp.task('assets', function() {
+  return gulp.src('src/assets/**/*.*')
+    .pipe(gulp.dest('./build/assets/'));
+});
+
+
